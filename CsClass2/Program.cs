eeaf50ee82b898be6db92e4d0079b5a3b8ac9951
dangmu723 <s2020@e-mirim.hs.kr>
2022-08-17 09:55:34 +0900

    초기 커밋.

---
+++ CsClass2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsClass2
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Wanted<string> wantedString = new Wanted<string>("String");
+            Wanted<int> wantedInt = new Wanted<int>(52273);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedDouble.Value);
+
+        }
+    }
+}
