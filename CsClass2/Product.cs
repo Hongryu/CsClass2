54a59fe4157a1db20d7da1d5ce63ccee6429f854
dangmu723 <s2020@e-mirim.hs.kr>
2022-09-29 13:06:44 +0900

    .

---
+++ CsClass2/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsClass2
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public int CompareTo(Product other)
+        {
+            int result = 0;
+            if(this.Price < other.Price)
+            {
+                result = -1;
+            }else if(this.Price > other.Price)
+            {
+                result = 1;
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            return Name + " : " + "원";
+        }
+    }
+}
